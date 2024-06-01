90e161cb70bb72c5dbedbccb84f2cfbc1e2028f3
Miladshahhi <Miladshahhi@users.noreply.github.com>
2024-06-01 12:47:45 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Miladshahhi"
+original_name="webchat"
+original_urlname="webchat"
+original_description="Awesome webchat created by Miladshahhi"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv webchat $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
